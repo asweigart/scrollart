9426f599aafa634f85d01ab9192e3484e14c5a69
Al Sweigart <asweigart@gmail.com>
2024-05-06 09:40:25 -0400

    inchworm varialbe rename

--- python/inchworm.py
+++ python/inchworm.py
@@ -5,19 +5,19 @@ DELAY = 0.5
 
 WORM_LENGTH = 3
 
-stretched_body = 'o' + ('o' * (WORM_LENGTH * 2)) + 'o'
-bunched_body = 'o' + ('O' * WORM_LENGTH) + 'o'
+STRETCHED_BODY = 'o' + ('o' * (WORM_LENGTH * 2)) + 'o'
+BUNCHED_BODY = 'o' + ('O' * WORM_LENGTH) + 'o'
 indent = 0
 while indent + WORM_LENGTH * 2 + 2 < WIDTH:
     backspaces_str = "\b" * (indent + WORM_LENGTH * 2 + 2)
 
     indentation_str = ' ' * indent
-    print(backspaces_str + indentation_str + stretched_body, flush=True, end='')
+    print(backspaces_str + indentation_str + STRETCHED_BODY, flush=True, end='')
     time.sleep(DELAY)
 
     indent += WORM_LENGTH
 
     indentation_str = ' ' * indent
-    print(backspaces_str + indentation_str + bunched_body, flush=True, end='')
+    print(backspaces_str + indentation_str + BUNCHED_BODY, flush=True, end='')
     time.sleep(DELAY)
 print()
