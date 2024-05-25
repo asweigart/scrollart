2e5974ab1b5bca1758aa3c5e631741a585ac7f6d
Al Sweigart <asweigart@gmail.com>
2024-05-25 16:08:44 -0400

    Updating forth and back

--- python/forthandback.py
+++ python/forthandback.py
@@ -1,40 +1,62 @@
 import random, time, os, math
 
-
 WIDTH = os.get_terminal_size()[0] - 1
+DELAY = 0.01
 
-DELAY = 0.1
+steps_before_switch = 20
+
 BLOCK_CHAR = '#'
-BLOCK_HEIGHT = 10
-BLOCK_WIDTH = 20
-INTERSPACE_HEIGHT = 40
+BLOCK_WIDTH = 14
 
-MIN_BOUNCES_PER_CHANGEUP = 1
-MAX_BOUNCES_PER_CHANGEUP = 6
-
-
-def easeOutBounceTween(n):
-    if n < (1 / 2.75):
-        return 7.5625 * n * n
-    elif n < (2 / 2.75):
-        n -= 1.5 / 2.75
-        return 7.5625 * n * n + 0.75
-    elif n < (2.5 / 2.75):
-        n -= 2.25 / 2.75
-        return 7.5625 * n * n + 0.9375
-    else:
-        n -= 2.65 / 2.75
-        return 7.5625 * n * n + 0.984375
-
-def easeInBounceTween(n):
-    return 1 - easeOutBounceTween(1 - n)
+empty_chars = list('~                                          ')
 
 try:
-    on_left = True
+    pos = 0
+    speed = 0
+    step = 0
+    total_steps = 0
+    direction = 'right'
     while True:
+        if step > steps_before_switch and direction == 'right':
+            speed += 1
+            pos += speed
+            if pos > WIDTH - BLOCK_WIDTH:
+                pos = WIDTH - BLOCK_WIDTH
+                step = 0
+                speed = 0
+                direction = 'left'
+                steps_before_switch = random.randint(10, 40)  # was 20, 70
+                if len(empty_chars) > 4:
+                    empty_chars.pop()
+                    empty_chars.pop()
+                    empty_chars.pop()
+        elif step > steps_before_switch and direction == 'left':
+            speed -= 1
+            pos += speed
+            if pos < 0:
+                pos = 0
+                step = 0
+                speed = 0
+                direction = 'right'
+                steps_before_switch = random.randint(1, 20)   # was 10, 40
+                if len(empty_chars) > 4:
+                    empty_chars.pop()
+                    empty_chars.pop()
+                    empty_chars.pop()
 
-        # Bounce to other side:
-        if 
+        for i in range(BLOCK_WIDTH // 2):
+            columns = [random.choice(empty_chars) for i in range(WIDTH)]
+            for i in range(pos, pos + BLOCK_WIDTH):
+                columns[i] = BLOCK_CHAR
+            print(''.join(columns))
+        time.sleep(DELAY)
+        
+        step += 1
+        total_steps += 1
+        if total_steps == 1000:
+            total_steps = 0
+            empty_chars = list('~                                          ')
 
 except KeyboardInterrupt:
     print('Forth and Back by Al Sweigart al@inventwithpython.com 2024')
+    
